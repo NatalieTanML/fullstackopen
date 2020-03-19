a6ab9c88aeb3523730f9024056258014543ad524
NatalieTanML <nat130613@gmail.com>
2020-03-19 21:10:42 +0800

    part1 tutorial

---
+++ part1/tutorial/src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Hello = props => {
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  );
+};
+
+const App = () => {
+  const name = "Bob";
+  const age = 10;
+
+  return (
+    <div>
+      <p>Greetings</p>
+      <Hello name="George" age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
